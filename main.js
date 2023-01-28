876039a995a370483122633e2382254e45a40bb8
Jorge Galdino <jorgegaldino1997@gmail.com>
2023-01-28 10:43:35 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,32 @@
+const form = document.getElementById('tarefa-nova'); 
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const inputNomeTarefa = document.getElementById('tarefa');
+
+    let linha = '<li>';
+    linha += `<div>${inputNomeTarefa.value}</div>`;
+    linha += '</li>';
+
+    linhas += linha;
+
+    const corpoUl = document.querySelector('ul');
+    corpoUl.innerHTML = linhas;
+})
+
+$(document).ready(function(){
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp();
+    })
+
+    $('a').click(function() {
+        $('ul')
+    })
+
+})
